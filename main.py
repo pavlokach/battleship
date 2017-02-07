2a43dac65522a6b481f4fa793eac0e7470cf45df
pavlokach <kachmar@ucu.edu.ua>
2017-02-07 14:11:29 +0200

    Update and rename lab1.py to main.py

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
+# My data type is tuple of two lists.
+# First list contains unshooted cells.
+# Second contains shooted cells.
+# Also you can see two fields: field.txt is correct,
+# fail_field.txt is incorrect.
 import string
 alphabet = string.ascii_uppercase
 
 
 def read_field(fname):
+    '''
+
+    (str) -> (data)
+
+    Takes file name and returns data.
+    '''
     import string
     alphabet = string.ascii_uppercase
-
+    stars = []
+    shots = []
     lines = []
     with open(fname, encoding='UTF-8') as field:
         for line in field:
             lines.append(line)
-    stars = []
-    shots = []
-
     for i in range(len(lines)):
         for j in range(len(lines[i])):
             if lines[i][j] == '*':
@@ -23,14 +32,28 @@ def read_field(fname):
 
 
 def has_ship(data, coords):
+    '''
+
+    (data, list) -> (bool)
+
+     Returns True if coords in data else False.
+    '''
     return True if coords in data[0] or coords in data[1] else False
 
 
 def ship_size(data, coords):
+    '''
+
+    (data, list) -> (int/False)
+
+    Returns False if coordinates aren`t in field else returns ship size.
+    '''
+    stars = data[0] + data[1]
+    if has_ship(data, coords) is False:
+        return False
     ver, hor = False, False
     size = 1
     fine = []
-    stars = data[0] + data[1]
     coords = list(coords)
     for star in stars:
         if star[0] == coords[0] or star[1] == coords[1]:
@@ -63,6 +86,10 @@ def ship_size(data, coords):
 
 
 def touch_check(data, coords):
+    '''
+    (data, list) -> (bool)
+    Being used in is_valid(data) to ckeck if two ships aren`t touching.
+    '''
     stars = data[0] + data[1]
     hor, ver = False, False
     if (coords[0], coords[1] + 1) in stars \
@@ -81,13 +108,16 @@ def touch_check(data, coords):
 
 
 def is_valid(data):
+    '''
+    (data) -> (bool)
+    Checks if field is valid to play.
+    '''
     stars = data[0] + data[1]
     ships_len = []
     if len(stars) != 20:
         return False
     for star in stars:
         ships_len.append(ship_size(data, star))
-    print(ships_len)
     if ships_len.count(2) != 6 or ships_len.count(1) != 4 \
        or ships_len.count(3) != 6 or ships_len.count(4) != 4:
         return False
